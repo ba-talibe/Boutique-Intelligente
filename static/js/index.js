fc87ea578397b6fce373b383eaf726a2b0e4bfb8
ba-talibe <omarba2015@gmail.com>
2022-06-29 01:03:40 +0000

    flask static files

---
+++ static/js/index.js
@@ -0,0 +1,134 @@
+let buttons = document.getElementsByTagName("button");
+
+function startCamera(){
+    navigator.mediaDevices.getUserMedia({
+        audio : false,
+        video : true,
+        width: 640, 
+        height: 480
+    }).then( (stream) => {
+        var video = document.getElementsByTagName('video')[0];
+        video.srcObject = stream;
+        video.play();
+    })
+    
+    let video = document.querySelector("#video")
+    video.style.width = "70%";
+    video.style.height = "auto";
+}
+
+function base64ToBlob(base64, mime) 
+{
+    mime = mime || '';
+    var sliceSize = 1024;
+    var byteChars = window.atob(base64);
+    var byteArrays = [];
+
+    for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
+        var slice = byteChars.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    return new Blob(byteArrays, {type: mime});
+}
+
+
+function customBase64Encode(inputStr) {
+    var
+        bbLen               = 3,
+        enCharLen           = 4,
+        inpLen              = inputStr.length,
+        inx                 = 0,
+        jnx,
+        keyStr              = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                            + "0123456789+/=",
+        output              = "",
+        paddingBytes        = 0;
+    var
+        bytebuffer          = new Array (bbLen),
+        encodedCharIndexes  = new Array (enCharLen);
+
+    while (inx < inpLen) {
+        for (jnx = 0;  jnx < bbLen;  ++jnx) {
+            /*--- Throw away high-order byte, as documented at:
+              https://developer.mozilla.org/En/Using_XMLHttpRequest#Handling_binary_data
+            */
+            if (inx < inpLen)
+                bytebuffer[jnx] = inputStr.charCodeAt (inx++) & 0xff;
+            else
+                bytebuffer[jnx] = 0;
+        }
+
+        /*--- Get each encoded character, 6 bits at a time.
+            index 0: first  6 bits
+            index 1: second 6 bits
+                        (2 least significant bits from inputStr byte 1
+                         + 4 most significant bits from byte 2)
+            index 2: third  6 bits
+                        (4 least significant bits from inputStr byte 2
+                         + 2 most significant bits from byte 3)
+            index 3: forth  6 bits (6 least significant bits from inputStr byte 3)
+        */
+        encodedCharIndexes[0] = bytebuffer[0] >> 2;
+        encodedCharIndexes[1] = ( (bytebuffer[0] & 0x3) << 4)   |  (bytebuffer[1] >> 4);
+        encodedCharIndexes[2] = ( (bytebuffer[1] & 0x0f) << 2)  |  (bytebuffer[2] >> 6);
+        encodedCharIndexes[3] = bytebuffer[2] & 0x3f;
+
+        //--- Determine whether padding happened, and adjust accordingly.
+        paddingBytes          = inx - (inpLen - 1);
+        switch (paddingBytes) {
+            case 1:
+                // Set last character to padding char
+                encodedCharIndexes[3] = 64;
+                break;
+            case 2:
+                // Set last 2 characters to padding char
+                encodedCharIndexes[3] = 64;
+                encodedCharIndexes[2] = 64;
+                break;
+            default:
+                break; // No padding - proceed
+        }
+
+        /*--- Now grab each appropriate character out of our keystring,
+            based on our index array and append it to the output string.
+        */
+        for (jnx = 0;  jnx < enCharLen;  ++jnx)
+            output += keyStr.charAt ( encodedCharIndexes[jnx] );
+    }
+    return output;
+}
+
+async function takeCapture(){
+    let canvas = document.createElement("canvas")
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height)
+    let image = canvas.toDataURL('image/jpeg')
+    var base64ImageContent = image.replace(/^data:image\/(png|jpeg);base64,/, "");
+    var blob = base64ToBlob(base64ImageContent, 'image/png'); 
+    let form = document.getElementsByTagName("form")[0]
+    let formData = new FormData(form);
+    formData.append("file", base64ImageContent);
+    let request = new XMLHttpRequest();
+    request.open("POST", "/image");
+    request.send(formData)
+    request.onload = () =>{
+        var response = JSON.parse(request.responseText)
+
+        console.log("response : ", response)
+        let url = `/inventaire?img=${response["imgPath"]}&produits=${response["produits"]}`;
+        if (confirm(response["produits"])){
+            window.location.replace(url)
+        }
+    }
+}
+
+
+startCamera()
